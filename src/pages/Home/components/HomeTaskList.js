f1fec1aff1c7f7e54dc39a13cd70be33b06678bc
Wilmer <wilmer.payall@gmail.com>
2020-06-20 21:14:05 -0400

    add message to empty list

--- src/pages/Home/components/HomeTaskList.js
+++ src/pages/Home/components/HomeTaskList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import HomeTaskListItemContainer from '../container/HomeTaskListItemContainer';
 
+const HomeTaskEmptyList = () => (
+  <div className='card'>
+    <div className='card-body text-center'>
+     Aún no has agregado tareas
+    </div>
+  </div>
+);
+
 const HomeTaskList = ({ tasks, users }) => (
   <div className='todo-home__task-list mt-2'>    
-    {tasks.tasksList.items.map(task => (
+    {!tasks.tasksList.items.length ? <HomeTaskEmptyList /> : tasks.tasksList.items.map(task => (
       <HomeTaskListItemContainer 
         key={task.id}
         task={task}
