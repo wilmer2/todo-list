37a3fe4636e10877f48e48567b86a1f45dbfe6fb
Wilmer <wilmer.payall@gmail.com>
2020-06-21 13:44:06 -0400

    seperate add button task

---
+++ src/pages/Home/components/AddTaskButton.js
@@ -0,0 +1,17 @@
+import LoaderSpinner from '../../../components/ui/LoaderSpinner';
+
+const AddTaskButton = ({ loading }) => (
+  <button 
+    type='submit' 
+    className='btn btn-primary btn-sm ml-2'
+    disabled={loading}
+  >
+    {!loading ? 'Agregar' : <LoaderSpinner 
+      height='20' 
+      width='50' 
+      color='white'
+    />}
+  </button>
+);
+
+export default AddTaskButton;
--- src/pages/Home/components/HomeInput.js
+++ src/pages/Home/components/HomeInput.js
@@ -1,19 +1,5 @@
 import React from 'react';
-import LoaderSpinner from '../../../components/ui/LoaderSpinner';
-
-const AddTaskButton = ({ loading }) => (
-  <button 
-    type='submit' 
-    className='btn btn-primary btn-sm ml-2'
-    disabled={loading}
-  >
-    {!loading ? 'Agregar' : <LoaderSpinner 
-      height='20' 
-      width='50' 
-      color='white'
-    />}
-  </button>
-);
+import AddTaskButton from './AddTaskButton';
 
 const HomeInput = ({
   onChangeTaskName,
