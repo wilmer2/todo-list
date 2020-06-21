1a1cacf5a91c245c01535bc722cc9dfe42e4db19
Wilmer <wilmer.payall@gmail.com>
2020-06-20 23:10:17 -0400

    adding todo-home__task-dropdown-item class

--- src/pages/Home/components/HomeDropDownItem.js
+++ src/pages/Home/components/HomeDropDownItem.js
@@ -11,7 +11,7 @@ const HomeDropDownItem = ({ user, onChangeUser, onClickShowDropDown }) => {
   
   return (
     <div 
-      className='d-flex align-items-center mt-2 pt-1 pb-2 pl-4 pr-4 border-bottom todo__pointer'
+      className='d-flex align-items-center mt-2 pt-1 pb-2 pl-4 pr-4 border-bottom todo__pointer todo-home__task-dropdown-item'
       onClick={handleClick}
     >
       <figure className='todo-home__task-item-img-container'>
