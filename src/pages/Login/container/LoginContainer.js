a8162b932a1b0c7cf154482f1ced5484971192e3
Wilmer <wilmer.payall@gmail.com>
2020-06-23 15:31:58 -0400

    resolve bug login user firebase

--- src/pages/Login/container/LoginContainer.js
+++ src/pages/Login/container/LoginContainer.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql } from 'apollo-boost';
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation, useLazyQuery } from '@apollo/react-hooks';
 import isEmpty from 'lodash/isEmpty';
 import parseError from '../../../utils/parseError';
 import guestUserEnhancer from '../../../components/hoc/GuestUserEnhancer';
@@ -17,8 +17,17 @@ const SEND_CREDENTIALS = gql`
   }
 `;
 
+const FIND_USER_BY_EMAIL = gql`
+  query FindUserByEmail($email: String) {
+    user(email: $email) {
+      id
+    }
+  }
+`;
+
 
 const LoginContainer = ({ history }) => {
+  const [findUserByEmail, { data: user, error: errorUser }] = useLazyQuery(FIND_USER_BY_EMAIL);
   const [sendCredentials, { loading }] = useMutation(SEND_CREDENTIALS);
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -30,8 +39,9 @@ const LoginContainer = ({ history }) => {
       
       await localStorage.setItem('token', userLogin.auth.idToken);
       await localStorage.setItem('refreshToken', userLogin.auth.refreshToken);
-      await localStorage.setItem('email', values.email)
-      history.push('/home');
+      await localStorage.setItem('email', values.email);
+      await findUserByEmail({ variables: { email: values.email }});
+      
     } catch(error) {
       const parsedError = parseError(error);
 
@@ -42,7 +52,20 @@ const LoginContainer = ({ history }) => {
       }
     }
   }
-  
+
+  useEffect(() => {
+    if (errorUser) {
+      localStorage.clear();
+      setErrorMessage('Correo o contraseña incorrectos');
+    }
+  }, [errorUser, setErrorMessage, localStorage]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/home');
+    }
+  }, [user, history]);
+
   return (
     <LoginView onSubmit={handleOnSubmit} errorMessage={errorMessage}  loading={loading} />
   );
